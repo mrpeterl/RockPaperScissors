bd53d5a99c33f8ec4b9e1db23fcf1b59945e3602
Peter Lappin <peterl@kainos.com>
2017-10-03 11:30:45 +0100

    Added player & Interface class, along with main method **STILL IN PROGRESS**

---
+++ src/com/company/Player.java
@@ -0,0 +1,41 @@
+package com.company;
+import java.util.Scanner;
+
+public class Player {
+
+
+
+    private int currentLosses;
+    private int currentWins;
+    private String name;
+    public Player(String name){
+        this.name = name;
+        this.currentWins = 0;
+        this.currentLosses = 0;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCurrentLosses() {
+        return currentLosses;
+    }
+
+    public void setCurrentLosses(int currentLosses) {
+        this.currentLosses = currentLosses;
+    }
+
+    public int getCurrentWins() {
+        return currentWins;
+    }
+
+    public void setCurrentWins(int currentWins) {
+        this.currentWins = currentWins;
+    }
+}
